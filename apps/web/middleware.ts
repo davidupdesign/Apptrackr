1a50d277a72146dc0d80811ea723ed9f8069042a
David K <davidupdesign@gmail.com>
2026-03-16 18:16:10 +0400

    8.8 - redirected logged in users away from login and signup

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,17 +3,29 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   // reading the token from cookiues
   const token = request.cookies.get("token")?.value;
+  const { pathname } = request.nextUrl;
 
-  // if no toekn - redirect to login
-  if (!token) {
+  // if the user tries to access /dashboard or /applications without a token, redirect them to /login
+  const isAppRoute =
+    pathname.startsWith("/dashboard") || pathname.startsWith("/applications");
+
+  if (isAppRoute && !token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // if toekn - allow the req
+  // if user lreadt has a token and visits login or signupo, theyre alreadt authenticated , so we send them to dashboard
+  const isAuthRoute =
+    pathname.startsWith("/login") || pathname.startsWith("/signup");
+
+  if (isAuthRoute && token) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  // else
   return NextResponse.next();
 }
 
 //only running the middleware on these routes
 export const config = {
-  matcher: ["/dashboard/:path*", "/applications/:path*"],
+  matcher: ["/dashboard/:path*", "/applications/:path*", "/login", "/signup"],
 };
